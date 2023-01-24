aa72014546e6ddba478bf2a85f4fb048e056d97e
WouterPerd <122367669+WouterPerd@users.noreply.github.com>
2023-01-24 15:05:08 +0200

    OD/sh 2.0

--- OntologyDownload.sh
+++ OntologyDownload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+java validate sdg.ttl urls > URL.txt
+
 file="URL.txt" #file name
 
 IFS=' ' #setting space as delimiter
@@ -7,6 +9,12 @@ IFS=' ' #setting space as delimiter
 while read -r line
 do
 	read -a ARRAY <<< "$line" #reads line as an array as tokens separated by IFS
-	curl -L -H \"Accept: text/turtle\" ${ARRAY[1]} >> ${ARRAY[0]}.ttl #Creates the ontology files
+	if [ -f ${ARRAY[0]}.ttl ] #checks for duplicates
+	then
+		echo "DUPLICATE"
+	else
+		curl -L -H \"Accept: text/turtle\" ${ARRAY[1]} >> ${ARRAY[0]}.ttl #Creates the ontology files
+		java validate ${ARRAY[0]}.ttl output > ${ARRAY[0]}.txt
+	fi
 	unset ARRAY
 done <$file
